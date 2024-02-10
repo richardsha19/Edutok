1cc30c2ba18ae956e1750463a891c12d47aeee8d
Jonathan Polina <69684393+jpolina@users.noreply.github.com>
2024-02-10 01:37:55 -0500

    Add blank landing page and scrolling page

--- Frontend/edutok/src/App.js
+++ Frontend/edutok/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import LandingPage from './components/LandingPage';
+import ScrollingPage from './components/ScrollingPage';
+
 
 function App() {
+  const [responseData, setResponseData] = useState([])
+  const [stage, setStage] = useState([0])
+
   return (
     <div className="App">
-      hi
+      {stage == 0 ?
+      (<LandingPage></LandingPage>) : 
+      (<ScrollingPage></ScrollingPage>)}
     </div>
   );
 }
---
+++ Frontend/edutok/src/components/ScrollingPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ScrollingPage = () => {
+  return (
+    <div>ScrollingPage</div>
+  )
+}
+
+export default ScrollingPage
