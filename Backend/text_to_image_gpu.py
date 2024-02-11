be30f29079af3c72fdce971d9567aa62c29f2073
richardsha19 <113735719+richardsha19@users.noreply.github.com>
2024-02-10 21:11:40 -0500

    Incorporated pdf generation

--- Backend/text_to_image_gpu.py
+++ Backend/text_to_image_gpu.py
@@ -4,10 +4,8 @@ import time
 import numpy as np
 from Text2ImgModel import Text2ImgModel
 
-model_cache = {}
 
-
-def prompt_to_image(prompt_text):
+def prompt_to_image(prompt_arr):
     output_dir = "output"
     model_ids = [
         "stabilityai/stable-diffusion-2-1",
@@ -49,12 +47,8 @@ def prompt_to_image(prompt_text):
         "pastoral"
     ]
 
-    if not prompt_text:
-        prompt_text = " "
-
-    if enhance_checkbox_value:
-        prompt_text = prompt_text + " " + " ".join(random.sample(enhancements, 5))
-        print(f"Using enhanced prompt: {prompt_text}")
+    if not prompt_arr:
+        prompt_arr = []
 
     for model_id in model_ids:
         model_key = (model_id, "cpu") # Changed from xpu
@@ -64,17 +58,25 @@ def prompt_to_image(prompt_text):
         model = model_cache[model_key]
 
         try:
-            start_time = time.time()
-            model.generate_images(
-                prompt_text,
-                num_images=num_images,
-                save_path="./output",
-            )
-            print(f"Complete generating {num_images} images in './output' in {time.time() - start_time:.2f} seconds.")
+            for prompt_text in prompt_arr:
+                if enhance_checkbox_value:
+                    prompt_text = prompt_text + " " + " ".join(random.sample(enhancements, 5))
+                    print(f"Using enhanced prompt: {prompt_text}")
+
+                start_time = time.time()
+                model.generate_images(
+                    prompt_text,
+                    num_images=num_images,
+                    save_path="./output",
+                )
+                print(f"Complete generating {num_images} images in './output' in {time.time() - start_time:.2f} seconds.")
         except Exception as e:
             print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    prompt = "Barack Obama giving his inauguration speech"
-    prompt_to_image(prompt)
+    pass
+    # Format: json file with key value pairs, the keys are the page numbers,
+    # prompt = ["Barack Obama giving his inauguration speech", "The fall of the Berlin Wall.",
+    #           "A depiction of the Industrial Revolution, focusing on its impact on rural communities.", "the Battle of Gettysburg from the viewpoint of a Confederate soldier"]
+
